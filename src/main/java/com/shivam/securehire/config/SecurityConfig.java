915e460ab0c55b5ca1239afd1644acc340d7128b
Shivam Singh <shivambd7.8.6@gmail.com>
2026-04-17 17:47:36 +0530

    Added Authentication Manager

--- src/main/java/com/shivam/securehire/config/SecurityConfig.java
+++ src/main/java/com/shivam/securehire/config/SecurityConfig.java
@@ -70,6 +70,8 @@ package com.shivam.securehire.config;
 import com.shivam.securehire.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -84,6 +86,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
--- src/main/java/com/shivam/securehire/controller/AuthController.java
+++ src/main/java/com/shivam/securehire/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.shivam.securehire.controller;
-
+import org.springframework.security.core.Authentication;
 import com.shivam.securehire.model.dto.LoginRequest;
 import com.shivam.securehire.model.dto.LoginResponse;
 import com.shivam.securehire.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,24 +14,27 @@ public class AuthController {
 
     @Autowired
     private JwtUtil jwtUtil;
-
+    @Autowired
+    private AuthenticationManager authenticationManager;
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request) {
 
-        if ("shivam".equals(request.getUsername()) &&
-                "user1234".equals(request.getPassword())) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getUsername(),
+                            request.getPassword()
+                    )
+            );
 
-            String token = jwtUtil.generateToken(request.getUsername());
-            return new LoginResponse(token, request.getUsername(), "Login success");
+            String username = authentication.getName();
+
+            String token = jwtUtil.generateToken(username);
+
+            return new LoginResponse(token, username, "Login success");
+
+        } catch (Exception e) {
+            return new LoginResponse(null, null, "Invalid credentials");
         }
-
-        if ("admin".equals(request.getUsername()) &&
-                "admin123".equals(request.getPassword())) {
-
-            String token = jwtUtil.generateToken(request.getUsername());
-            return new LoginResponse(token, request.getUsername(), "Admin login success");
-        }
-
-        return new LoginResponse(null, null, "Invalid credentials");
     }
 }
